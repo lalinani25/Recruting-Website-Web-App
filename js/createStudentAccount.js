be204d48cce76b772b715ce706197626b8b8346b
Laura Alinani <lalinani@eagles.bridgewater.edu>
2024-10-29 18:18:33 -0400

    new update

---
+++ js/createStudentAccount.js
@@ -0,0 +1,61 @@
+const signUp = document.querySelector('#studentSignUp');
+const message = document.querySelector("p");
+
+signUp.addEventListener('click', async function (event) {
+
+    const email = document.querySelector('#student_email').value;
+    const name = document.querySelector('#student_name').value;
+    const password = document.querySelector('#student_password').value;
+    const school = document.querySelector('#student_school').value;
+    const gradYear = document.querySelector('#student_gradYear').value;
+
+
+    const userData = {
+        email: email,
+        name: name,
+        password: password,
+        school: school,
+        gradYear: gradYear,
+    };
+
+
+    await createAccount(userData);
+
+});
+
+async function createAccount(userData) {
+    const mssg = document.querySelector("p");
+    const url = "https://football-api-server-ane2c4bwdacvf8f6.eastus-01.azurewebsites.net/studentuser";
+    console.log("test")
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userData)
+        });
+
+        if (response.ok) {
+            try {
+                const data = await response.json();
+                mssg.innerHTML = data.message;
+                message.textContent = 'Verification email has been sent to ' + userData.email;
+                message.style.color = 'white';
+            } catch (error) {
+                console.error('Error parsing JSON response:', error.message);
+                mssg.innerHTML = 'Verification email has been sent to ' + userData.email;
+                mssg.style.color = 'white';
+            }
+        } else {
+            const errorData = await response.json();
+            mssg.innerHTML = "Error: " + errorData.message;
+            mssg.style.color = 'red';
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        mssg.innerHTML = "Error: " + error.message;
+        mssg.style.color = 'red';
+    }
+}
+
